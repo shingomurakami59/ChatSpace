d9dfbb1d919375d5caf7be98f8a3f4785bc83012
shingomurakami59 <joyfut10shingo@gmail.com>
2019-10-04 16:47:44 +0900

    Haml実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root "groups#index"
+
   resources :users, only: %i(index edit update)
   resources :groups, only: %i(new create edit update) do
     resources :messages, only: %i(index create)
