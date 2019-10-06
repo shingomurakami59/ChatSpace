b308614f24fd877dc497801de2b6968b7b0705b5
shingomurakami59 <joyfut10shingo@gmail.com>
2019-10-06 14:41:42 +0900

    Create routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  root 'messages#index'   #ルートパスの指定
+  resources :messages
+  resources :groups
+  resources :users, only: [:show]
+end
+
+
